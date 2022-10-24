1c656969490459eaff7d28ae1e73f4964c7f15f9
Amado Melguizo <56071729+Amado-Melguizo@users.noreply.github.com>
2022-10-24 23:52:46 +0200

    Add files via upload

---
+++ webMusica/configuration.inc.php
@@ -0,0 +1,29 @@
+<?php
+$DB_SERVER="localhost";     /* Servidor de la BD */
+$DB_NAME="music_stock";     /* Base de datos de la BD */
+$DB_USER="user";           /* Contraseña de la BD */  
+$DB_PASS ="user1234";           /* Usuario de la BD */
+?>
+
+
+<!-- 
+Usar Mysql en Windows 
+
+cd \xampp\mysql\bin\
+mysql.exe –h localhost –u root -p
+
+//////////////////////////////////////////
+
+Pasos para usuarios BD;
+- Crear ususario:
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'user1234';
+
+- Dar permisos:
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
+
+- Modificar usuario:
+ALTER USER 'user'@'localhost' IDENTIFIED BY '1234' 
+-->
+
+
+
